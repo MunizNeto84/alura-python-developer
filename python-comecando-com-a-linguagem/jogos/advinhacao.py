ce5004413cf9325af0faefde4bfe4b97b28f887c
munizneto84 <muniznetto@outlook.com>
2022-07-14 23:29:58 -0300

    jogo de advinhacao - melhorando o codigo

--- python-comecando-com-a-linguagem/jogos/advinhacao.py
+++ python-comecando-com-a-linguagem/jogos/advinhacao.py
@@ -1,1 +1,24 @@
-print ('bem vindo no jono de advinhação')
+print('********************************')
+print ('bem vindo no jogo de advinhação')
+print('********************************')
+
+num_secreto = 42
+
+chute_str = input('Digite o seu numero')
+print('Você digitou ', chute_str)
+
+chute = int(chute_str)
+acertou = chute == num_secreto
+chute_maior = chute > num_secreto
+chute_menor = chute < num_secreto
+
+
+if (acertou):
+    print('você acertou')
+else:
+    if (chute_maior):
+        print('você errou! o chute foi maior do que o numero secreto.')   
+    elif(chute_menor):
+        print('você errou! o seu chute foi menor que o numero secreto')    
+
+print('fim do jogo')

2d2d851997481e54645c17e09529b1d15d2099de
munizneto84 <muniznetto@outlook.com>
2022-07-19 22:42:45 -0300

    iterando de maneira diferente

--- python-comecando-com-a-linguagem/jogos/advinhacao.py
+++ python-comecando-com-a-linguagem/jogos/advinhacao.py
@@ -3,22 +3,29 @@ print ('bem vindo no jogo de advinhação')
 print('********************************')
 
 num_secreto = 42
+total_de_tentativas = 3
 
-chute_str = input('Digite o seu numero')
-print('Você digitou ', chute_str)
+for rodada in range (1, total_de_tentativas+1):
+    print('Tentativa {} de {}'.format(rodada, total_de_tentativas))
+    chute_str = input('Digite o seu número de 1 a 100: ')
+    print('Você digitou ', chute_str)
 
-chute = int(chute_str)
-acertou = chute == num_secreto
-chute_maior = chute > num_secreto
-chute_menor = chute < num_secreto
+    if(chute <1):
+        print('Você deve digitar um numero de 1 a 100')    
+
+    chute = int(chute_str)
+    acertou = chute == num_secreto
+    chute_maior = chute > num_secreto
+    chute_menor = chute < num_secreto
 
 
-if (acertou):
-    print('você acertou')
-else:
-    if (chute_maior):
-        print('você errou! o chute foi maior do que o numero secreto.')   
-    elif(chute_menor):
-        print('você errou! o seu chute foi menor que o numero secreto')    
+    if (acertou):
+        print('você acertou')
+        break
+    else:
+        if (chute_maior):
+            print('você errou! o chute foi maior do que o numero secreto.')   
+        elif(chute_menor):
+            print('você errou! o seu chute foi menor que o numero secreto')    
 
 print('fim do jogo')

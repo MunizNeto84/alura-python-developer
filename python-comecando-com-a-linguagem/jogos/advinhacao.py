03b1a0779b4742f9b72bfdd81756672c375fd01a
munizneto84 <muniznetto@outlook.com>
2022-07-19 22:58:57 -0300

    gerando numeros aleatórios

--- python-comecando-com-a-linguagem/jogos/advinhacao.py
+++ python-comecando-com-a-linguagem/jogos/advinhacao.py
@@ -1,19 +1,23 @@
+import random
+
 print('********************************')
 print ('bem vindo no jogo de advinhação')
 print('********************************')
 
-num_secreto = 42
+num_secreto = random.randrange(1,101)
 total_de_tentativas = 3
 
 for rodada in range (1, total_de_tentativas+1):
     print('Tentativa {} de {}'.format(rodada, total_de_tentativas))
     chute_str = input('Digite o seu número de 1 a 100: ')
     print('Você digitou ', chute_str)
-
-    if(chute <1):
-        print('Você deve digitar um numero de 1 a 100')    
-
     chute = int(chute_str)
+
+    if(chute < 1 or chute > 100):
+        print('Você deve digitar um numero de 1 a 100')
+        continue
+    
+
     acertou = chute == num_secreto
     chute_maior = chute > num_secreto
     chute_menor = chute < num_secreto

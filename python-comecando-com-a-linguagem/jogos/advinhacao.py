b49ba6221820d4f2dd1e84e6a97a35d2e548026d
munizneto84 <muniznetto@outlook.com>
2022-07-21 23:16:05 -0300

    niveis e pontuação

--- python-comecando-com-a-linguagem/jogos/advinhacao.py
+++ python-comecando-com-a-linguagem/jogos/advinhacao.py
@@ -5,7 +5,20 @@ print ('bem vindo no jogo de advinhação')
 print('********************************')
 
 num_secreto = random.randrange(1,101)
-total_de_tentativas = 3
+total_de_tentativas = 0
+pontos = 1000
+
+print('Qual nivel de dificuldade?')
+print('1- Facil 2- Médio 3- Dificil')
+nivel = input('Define o nivel: ')
+
+if(nivel==1):
+    total_de_tentativas = 20
+elif(nivel==2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5      
+
 
 for rodada in range (1, total_de_tentativas+1):
     print('Tentativa {} de {}'.format(rodada, total_de_tentativas))
@@ -24,12 +37,16 @@ for rodada in range (1, total_de_tentativas+1):
 
 
     if (acertou):
-        print('você acertou')
+        print('você acertou e fez {} pontos!'.format(pontos))
         break
     else:
         if (chute_maior):
             print('você errou! o chute foi maior do que o numero secreto.')   
         elif(chute_menor):
-            print('você errou! o seu chute foi menor que o numero secreto')    
+            print('você errou! o seu chute foi menor que o numero secreto') 
+        pontos_perdidos = abs(num_secreto - chute)
+        pontos = pontos - pontos_perdidos
+
+
 
 print('fim do jogo')
